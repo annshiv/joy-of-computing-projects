96e951dabb1bd1fb76c2c9affd4ff129324a059f
Annshiv <annamalaipalani11@gmail.com>
2020-10-14 00:34:31 +0530

    magic square problem week 4

--- magic_square.py
+++ magic_square.py
@@ -5,24 +5,39 @@ def magic_square(n):
         for j in range(n):
             l.append(0)
         square.append(l)
-    
-    for i in range(n):
-      for j in range(n):
-        print(square[i][j],end=" ")
-      print()
 
     m = (n*((n**2)+1))/2
     
-    row = int(n/2)
+    row = n//2
     col = n-1
 
     count = 1
     num = n*n
 
     while count <= num:
-        if count == 1:
-            return square[row][col] + 1
-            count += 1
-    print(square)
+        if (row == -1 and col == n):
+          col = n -2
+          row = 0
+        else:
+          if (col == n):
+            col = 0
+          if (row < 0):
+            row = n - 1
+        if(square[row][col] != 0):
+            col = col - 2
+            row = row + 1
+            continue
+        else:
+          square[row][col] = count
+          count += 1
+        row -= 1
+        col += 1
 
-magic_square(3)
+    for i in range(n):
+      for j in range(n):
+        print(square[i][j],end=" ")
+      print()
+
+if __name__ == "__main__":
+    n = int(input("Enter a odd number for magiic square: "))
+    magic_square(n)
