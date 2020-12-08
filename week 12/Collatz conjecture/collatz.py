4a50956fa056feeed7040cc2db489de55713c5c4
Annshiv <annamalaipalani11@gmail.com>
2020-12-08 20:00:22 +0530

    collatz collective

---
+++ week 12/Collatz conjecture/collatz.py
@@ -0,0 +1,13 @@
+def collatz(n):
+    count = 0
+    while n != 1:
+        if n % 2 == 0:
+            n = n/2
+            count += 1 
+        else:
+            n = (n * 3) + 1
+            count += 1
+    return n,count
+
+n = int(input())
+print(collatz(n))
